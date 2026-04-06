4754adfcfc39d7377df6bbc29399a6a1f3a23734
shifaKarnelia <shifakarnelia@gmail.com>
2026-04-06 19:46:44 -0400

    create lable to connect

--- A2_iOS_-shifa_101487402/ViewController.swift
+++ A2_iOS_-shifa_101487402/ViewController.swift
@@ -6,8 +6,19 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+
+        @IBOutlet weak var productIDValueLabel: UILabel!
+        @IBOutlet weak var productNameValueLabel: UILabel!
+        @IBOutlet weak var productDescriptionValueLabel: UILabel!
+        @IBOutlet weak var productPriceValueLabel: UILabel!
+        @IBOutlet weak var productProviderValueLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
