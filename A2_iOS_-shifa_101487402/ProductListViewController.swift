82bab5294aabd959785cdcfa52f4e84b7df38834
shifaKarnelia <shifakarnelia@gmail.com>
2026-04-08 19:16:05 -0400

    add product view list layout / connect class

---
+++ A2_iOS_-shifa_101487402/ProductListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProductListViewController.swift
+//  A2_iOS_-shifa_101487402
+//
+//  Created by Shifa Karnelia on 2026-04-08.
+//
+
+import CoreData
+import UIKit
+
+class ProductListViewController: UITableViewController {
+    
+}
