8704e6f2b6cd1ecdbb0b86e6f6a6f6346c990bbb
shifaKarnelia <shifakarnelia@gmail.com>
2026-04-07 22:07:52 -0400

    create AddProductViewController class and connect it

---
+++ A2_iOS_-shifa_101487402/AddProductViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddProductViewController.swift
+//  A2_iOS_-shifa_101487402
+//
+//  Created by Shifa Karnelia on 2026-04-07.
+//
+import UIKit
+import CoreData
+
+class AddProductViewController: UIViewController {
+    
+ 
+}
